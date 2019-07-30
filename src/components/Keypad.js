39b6c95ddf8f90eb13211a7585612e929c1dffbb
Tyler Hunt <tyhunt04@gmail.com>
2019-07-30 16:12:08 +0000

    Automatically backed up by Learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -5,7 +5,7 @@ export default class Keypad extends Component {
   upKeyPress = () => {
     console.log('Entering Password')
   }
-  
+
   render() {
     return (
       <input keyUp={this.upKeyPress} type="password" />
